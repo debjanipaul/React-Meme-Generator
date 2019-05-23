ea26b47107ecbafb6790ad835d27b11a34e45b31
Debjani Paul <debjanideb@hotmail.com>
2019-05-22 23:47:50 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
+import styles from './styles.css'
+function App() {
+
+    return (
+      <div>
+        <Header />
+        <MemeGenerator />
+      </div>
+    );
+}
+export default App;
